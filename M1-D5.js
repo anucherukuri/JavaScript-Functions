063e036874ae95bb13598161e6b551eaadd7d49a
Anudeep <36949642+anucherukuri@users.noreply.github.com>
2021-10-29 15:59:31 +0100

    Updated HW

--- M1-D5.js
+++ M1-D5.js
@@ -146,13 +146,19 @@ upperFirst("Hello")
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
+let newString
+const cutString = function(givenWord){
+    console.log("The given string without first and last word : "+ givenWord[])
 
+}
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 10
  Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
+const giveMeRandom = function(n){
 
+})
 /* WRITE YOUR ANSWER HERE */
 
 /* WHEN YOU ARE FINISHED
